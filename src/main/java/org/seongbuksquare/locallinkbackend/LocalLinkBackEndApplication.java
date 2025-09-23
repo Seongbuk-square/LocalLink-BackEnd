04dd6a1561f9fdaedcb621a7b5d8731db92bb380
simGPT <2023216057@skuniv.ac.kr>
2025-09-23 19:13:46 +0900

    :tada:Start: PBL 프로젝트 시작

---
+++ src/main/java/org/seongbuksquare/locallinkbackend/LocalLinkBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.seongbuksquare.locallinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalLinkBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalLinkBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/seongbuksquare/locallinkbackend/LocalLinkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seongbuksquare.locallinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalLinkBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
