6a219e2434193c5409a4b0e3c844a65469a24e92
심서현 <simseohyeon@simseohyeon-ui-MacBookPro.local>
2025-10-13 17:32:29 +0900

    :tada:Start: BaseTimeEntity 생성

---
+++ src/main/java/org/seongbuksquare/locallinkbackend/global/common/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package org.seongbuksquare.locallinkbackend.global.common;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate private LocalDateTime createdAt;
+
+    @LastModifiedDate private LocalDateTime modifiedAt;
+}
+
