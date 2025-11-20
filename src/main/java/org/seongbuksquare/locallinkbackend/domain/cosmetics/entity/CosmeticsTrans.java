e86febe661d752201c52a61bc32e1d9cb35d4c6a
심서현 <simseohyeon@simseohyeon-ui-MacBookPro.local>
2025-11-21 01:56:30 +0900

    :sparkles:Feat: CosmeticsTrans 엔티티 생성

--- src/main/java/org/seongbuksquare/locallinkbackend/domain/cosmetics/entity/CosmeticsTrans.java
+++ src/main/java/org/seongbuksquare/locallinkbackend/domain/cosmetics/entity/CosmeticsTrans.java
@@ -1,4 +1,29 @@
 package org.seongbuksquare.locallinkbackend.domain.cosmetics.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CosmeticsTrans {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cosmeticsTransId;
+
+    @Column(nullable = false)
+    private String languageCode;
+
+    @Column(nullable = false)
+    private String cosmeticsName;
+
+    @Column(nullable = false)
+    private String companyName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cosmetic_id", nullable = false)
+    private Cosmetics cosmetics;
 }
