b1d4e9963f8f73d71188bceef5a2f2885033cd27
simGPT <simGPT02@gmail.com>
2025-12-01 17:55:15 +0900

    :sparkles:Feat: KeywordsTrans entity 생성

--- src/main/java/org/seongbuksquare/locallinkbackend/domain/keywords/entity/KeywordsTrans.java
+++ src/main/java/org/seongbuksquare/locallinkbackend/domain/keywords/entity/KeywordsTrans.java
@@ -1,4 +1,31 @@
 package org.seongbuksquare.locallinkbackend.domain.keywords.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="keyword_trans")
 public class KeywordsTrans {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer keywordsTransId;
+
+    @Column(nullable = false)
+    private String languageCode;
+
+    @Column(nullable = false)
+    private String keywordName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyword_id", nullable = false)
+    private Keywords keywords;
+
 }
